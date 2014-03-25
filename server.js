7e72de1d7716dff603045329acb8e8c9846a3de1
farhaad <farhaad.bux@gmail.com>
2014-03-25 18:10:17 +0000

    Adjusting connection parameters to work on new domain

--- server.js
+++ server.js
@@ -3,13 +3,10 @@ var express  = require("express");
 var app = express();
 var db = require("./server_modules/dbconfig");
 
-var connection = db.start("127.12.201.2","nb","adminycZWy2b","esZFLqNQvlf9");
+var connection = db.start("107.170.89.145","nb","root","athens");
 //var connection = db.start("localhost","nb","root","");
-var port = process.env.OPENSHIFT_NODEJS_PORT ||  process.env.OPENSHIFT_INTERNAL_PORT || 8080;  
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || localhost;
 
 var config = require('./server_modules/config.js')(app, express);
 require('./server_modules/routes.js')(app, connection);
 
-
-http.createServer(app).listen(port, ipaddress);
+http.createServer(app).listen(80, "107.170.89.145");
