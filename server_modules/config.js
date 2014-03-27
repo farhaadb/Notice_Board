5741369feec0dc563db781e24dc970218d8656de
Farhaad <farhaad.bux@gmail.com>
2014-03-27 21:14:03 +0200

    Adding login with redis

--- server_modules/config.js
+++ server_modules/config.js
@@ -1,10 +1,21 @@
 module.exports = function(app, express){
 
 	app.configure(function() {
+	
+		var RedisStore = require('connect-redis')(express);
+		
 		app.use(express.static(__dirname + '/../client_modules')); 		// set the static files location /public/img will be /img for users
 		app.use(express.logger('dev')); 						// log every request to the console
 		app.use(express.bodyParser()); 							// pull information from html in POST
 		app.use(express.methodOverride()); 						// simulate DELETE and PUT
+		app.use(express.cookieParser());
+		app.use(express.session({ secret: 'password', 
+            store: new RedisStore({
+            host: '107.170.89.145',
+            port: '6379'                                         
+            })  
+        }));
+		app.use(app.router);
 	});
 
 };
