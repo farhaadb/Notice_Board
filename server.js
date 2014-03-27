a2aeca0741ef99261fd4582a604015e9eb491ef9
Farhaad <farhaad.bux@gmail.com>
2014-03-27 21:14:31 +0200

    Adding redis for login

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ var http = require("http");
 var express  = require("express");
 var app = express();
 var db = require("./server_modules/dbconfig");
+var auth = require("./server_modules/auth");
 
 var connection = db.start("107.170.89.145","nb","root","athens");
 //var connection = db.start("localhost","nb","root","");
 
+var auth_check = auth();
+
 var config = require('./server_modules/config.js')(app, express);
-require('./server_modules/routes.js')(app, connection);
+require('./server_modules/routes.js')(app, connection, auth_check);
 
 http.createServer(app).listen(80, "107.170.89.145");
+//http.createServer(app).listen(3000, "localhost");
