0122876e5d2e54bbdd9c3b573eb9f8da48f6f0d9
Farhaad <farhaad.bux@gmail.com>
2014-03-29 14:17:18 +0200

    Move db queries out of routing script, implement connection pooling

--- server_modules/routes.js
+++ server_modules/routes.js
@@ -1,40 +1,15 @@
-module.exports = function(app, connection, auth){
+module.exports = function(app, pool, auth, dbquery){
 
 	app.get("/", function(req, res){
 		res.sendfile("login.html");
 	});
 
-	app.post("/login.html", function(req,res){
-		connection.query("select count(*) as count from Login where username='" + req.body.user + "' and password='" + req.body.pass + "'",
-		function(err, rows, fields){
-			if(err){
-				console.log("error" + err);
-			}
-			
-			if(rows[0].count==1)
-			{
-				req.session.user_id="test";
-			}
-			
-			else
-			{
-				req.session.user_id=0;
-			}
-			res.redirect('/app');
-			console.log(JSON.stringify(rows));
-		});
+	app.post("/login.html", function(req, res){
+		dbquery.query(req, res, pool, "login");
 	});
 	
 	app.get("/gettable.html", function(req, res) {
-
-		connection.query('select notice_id as id,lecturer_id as empid, lname AS name, caption AS notice FROM Notice_LCS join Lecturer on Notice_LCS.lecturer_id=Lecturer.id join Notice on Notice_LCS.notice_id=Notice.id ORDER BY name',
-		function(err, rows, fields){
-			if(err){
-				console.log("error" + err);
-			}
-			res.send(JSON.stringify(rows));
-			console.log(JSON.stringify(rows));
-		});
+		dbquery.query(req, res, pool, "getNotices");
 	});
 	
 	app.get('/app', auth, function(req,res){
