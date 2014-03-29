fe59523944282e23084240da5e9f9316656ff430
Farhaad <farhaad.bux@gmail.com>
2014-03-29 18:11:28 +0200

    Adding dbquery custom module

--- server.js
+++ server.js
@@ -2,15 +2,14 @@ var http = require("http");
 var express  = require("express");
 var app = express();
 var db = require("./server_modules/dbconfig");
-var auth = require("./server_modules/auth");
+var auth = require("./server_modules/auth")();
+var dbquery = require("./server_modules/dbquery");
 
-var connection = db.start("107.170.89.145","nb","root","athens");
-//var connection = db.start("localhost","nb","root","");
-
-var auth_check = auth();
+var pool = db.start("107.170.89.145","nb","root","athens");
+//var pool = db.start("localhost","nb","root","");
 
 var config = require('./server_modules/config.js')(app, express);
-require('./server_modules/routes.js')(app, connection, auth_check);
+require('./server_modules/routes.js')(app, pool, auth, dbquery);
 
 http.createServer(app).listen(80, "107.170.89.145");
 //http.createServer(app).listen(3000, "localhost");
