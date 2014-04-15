b3fcbbda3f8025c0c274d635d0016fce2ccfda1f
Farhaad <farhaad.bux@gmail.com>
2014-04-15 20:25:21 +0200

    Adding multer for uploads

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ var app = express();
 var db = require("./server_modules/dbconfig");
 var auth = require("./server_modules/auth")();
 var dbquery = require("./server_modules/dbquery");
+var multer = require('multer');
 
 var pool = db.start("107.170.89.145","nb","root","athens");
 //var pool = db.start("localhost","nb","root","");
 
-var config = require('./server_modules/config.js')(app, express);
+var config = require('./server_modules/config.js')(app, express, multer);
 require('./server_modules/routes.js')(app, pool, auth, dbquery);
 
 http.createServer(app).listen(80, "107.170.89.145");
