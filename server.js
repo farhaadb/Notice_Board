074aeabad992e2b4eba6207ed05a9ed985bc6410
Farhaad <farhaad.bux@gmail.com>
2014-04-22 21:21:32 +0200

    Fix ip

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ var subdomain = require('subdomain');
 
 app.use(subdomain({ base : 'dutnoticeboard.co.za', removeWWW : true }));
 
-//var pool = db.start("107.170.89.145","nb","root","athens");
-var pool = db.start("localhost","nb","root","");
+var pool = db.start("107.170.89.145","nb","root","athens");
+//var pool = db.start("localhost","nb","root","");
 
 var config = require('./server_modules/config.js')(app, express, multer);
 require('./server_modules/routes.js')(app, pool, auth, dbquery, excelParser, fs, listDir);
 
-//http.createServer(app).listen(80, "107.170.89.145");
-http.createServer(app).listen(3000, "localhost");
+http.createServer(app).listen(80, "107.170.89.145");
+//http.createServer(app).listen(3000, "localhost");
