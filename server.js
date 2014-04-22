d48c86e86076e5bacdb8b3224e7ca030f2828b3d
Farhaad <farhaad.bux@gmail.com>
2014-04-22 21:17:34 +0200

    Testing subdomain

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ var multer = require('multer');
 var excelParser = require('excel-parser');
 var fs = require('fs');
 var listDir = require("./server_modules/directory");
+var subdomain = require('subdomain');
 
-var pool = db.start("107.170.89.145","nb","root","athens");
-//var pool = db.start("localhost","nb","root","");
+app.use(subdomain({ base : 'dutnoticeboard.co.za', removeWWW : true }));
+
+//var pool = db.start("107.170.89.145","nb","root","athens");
+var pool = db.start("localhost","nb","root","");
 
 var config = require('./server_modules/config.js')(app, express, multer);
 require('./server_modules/routes.js')(app, pool, auth, dbquery, excelParser, fs, listDir);
 
-http.createServer(app).listen(80, "107.170.89.145");
-//http.createServer(app).listen(3000, "localhost");
+//http.createServer(app).listen(80, "107.170.89.145");
+http.createServer(app).listen(3000, "localhost");
