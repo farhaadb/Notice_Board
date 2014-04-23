5b8f00445d4f2c6852bb2479882a6f80c6f60a9d
Farhaad <farhaad.bux@gmail.com>
2014-04-23 22:59:36 +0200

    Switching to busboy for uploading

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 var http = require("http");
+var path = require("path");
 var express  = require("express");
 var app = express();
 var db = require("./server_modules/dbconfig");
 var auth = require("./server_modules/auth")();
 var dbquery = require("./server_modules/dbquery");
-var multer = require('multer');
+var busboy = require('connect-busboy');
 var excelParser = require('excel-parser');
 var fs = require('fs');
 var listDir = require("./server_modules/directory");
 var subdomain = require('subdomain');
 
-app.use(subdomain({ base : 'dutnoticeboard.co.za', removeWWW : true }));
-
 var pool = db.start("107.170.89.145","nb","root","athens");
 //var pool = db.start("localhost","nb","root","");
 
-var config = require('./server_modules/config.js')(app, express, multer);
-require('./server_modules/routes.js')(app, pool, auth, dbquery, excelParser, fs, listDir);
+var config = require('./server_modules/config.js')(app, express, busboy, subdomain);
+require('./server_modules/routes.js')(app, pool, auth, dbquery, excelParser, fs, listDir, path);
 
 http.createServer(app).listen(80, "107.170.89.145");
-//http.createServer(app).listen(3000, "localhost");
+//http.createServer(app).listen(80, "127.0.0.1");
