4d9f790584e2d8a8a5216f3e472aa0fb1521e6ad
Farhaad <farhaad.bux@gmail.com>
2014-05-04 01:27:40 +0200

    Fix bug whereby the path does not change when a user accesses an empty folder, add functions to deal with removing files and folders, adding a folder and to perform checks for valid folder names, moving file view html out

--- lecturer/modules/component/Main.js
+++ lecturer/modules/component/Main.js
@@ -2,6 +2,10 @@
    
 function MainController($scope,$http ,myNotices,$window) {
 	$scope.statusmessage =  'Updating...';
+	var ip = "http://localhost:3000";
+	var path_history=[];
+	var disallowed_characters=["\\", "/", ":", "*", "?", "\"", "<", ">", "|"];
+	$scope.is_button_disabled=true;
 	$scope.reload =function(){
 	$window.location.reload();
 	$scope.dir;
@@ -10,7 +14,7 @@ function MainController($scope,$http ,myNotices,$window) {
 	//Makes call to factory to get lecturer id
 		function init(){
 				$scope.conn = true;
-				var url = 'http://10.0.0.10:3000/returnid';
+				var url = ip+'/returnid';
 				
 				myNotices.get(url).then(function(data) { //success
 					$scope.msg = data;
@@ -27,10 +31,10 @@ function MainController($scope,$http ,myNotices,$window) {
 		
 		};
 		
-		//--------------------Makes call to factory to get subjects-----------------------------
+		//--------------------Makes call to factory to get subjects-----------------------------//
 		function getSubjects(){
 		var d={'id' : $scope.lecturer_id};
-		var url = 'http://10.0.0.10:3000/returnsubjects';
+		var url = ip+'/returnsubjects';
 		
 		myNotices.post(url,d).then(function(subject) {
 						console.log(subject);
@@ -47,9 +51,9 @@ function MainController($scope,$http ,myNotices,$window) {
         		});
 		
 		};
-			//--------------------Makes call to factory to get notices----------------------------
+			//--------------------Makes call to factory to get notices----------------------------//
 		function getNotices(){
-		var url = 'http://10.0.0.10:3000/returnnotices';
+		var url = ip+'/returnnotices';
 		
 		myNotices.post(url,{'id':$scope.lecturer_id}).then(function(notices) {
 						console.log(notices);
@@ -64,13 +68,49 @@ function MainController($scope,$http ,myNotices,$window) {
 					$scope.conn = false;
         		});
 		
-		};
+		}
+		
+		$scope.deleteNotice = function(id){
+			console.log(id);
+				
+			var url = ip+'/deletenotice';
+				
+			myNotices.post(url,{'id':id}).then(function(notices) {
+					console.log(notices);
+					$scope.notices=notices;
+					console.log($scope.notices);
+					
+			},
+			function(data) { //failure
+				console.log('WE ARE HAVING TROUBLE RETRIEVING YOUR NOTICES');
+				$scope.statusmessage =  'WE ARE HAVING TROUBLE RETRIEVING YOUR NOTICES';
+				$scope.ready =true;
+				$scope.conn = false;
+        	});
+			
+		}
+		
+		//-------------------------------------------------------------------------------------//
+		//All functions pertaining to the directory listing
+		//-------------------------------------------------------------------------------------//
 		
 		
-		//--------------------Makes call to factory to update View-----------------------------
-		$scope.updateView = function(subject){
+		//--------------------Makes call to factory to update View-----------------------------//
+		$scope.updateView = function(subject, initial_view){
 		
-		if((document.getElementById("dropbox2").className) == "hidden")
+		initial_view = initial_view || false; //assigns false by default if no parameter is passed in
+		
+		if(initial_view)
+		{
+			document.getElementById("initial").className = "";
+			document.getElementById("file").className = "hidden";
+			document.getElementById("folder").className = "hidden";
+			document.getElementById("directory_options").className = "hidden";
+			return;
+		}
+		
+		if((document.getElementById("folder").className) == "hidden" && (document.getElementById("file").className) == "hidden"
+			&& (document.getElementById("directory_options").className) == "hidden")
 		{
 			var path = $scope.lecturer_id+"/subjects/"+subject;
 		}
@@ -80,101 +120,217 @@ function MainController($scope,$http ,myNotices,$window) {
 			var path = subject;
 		}
 		console.log(path);
-		var url = 'http://10.0.0.10:3000/dir';
+		
+		if(path_history.length>0) //don't store duplicates
+		{ console.log(path_history[(path_history.length-1)]+"==="+path);
+			if(path_history[(path_history.length-1)]!=path)
+			{
+				path_history.push(path);		
+			}
+		}
+		
+		else
+		{
+			path_history.push(path);
+		}
+		var url = ip+'/listDirectory';
 		
 		myNotices.post(url,{'path':path}).then(function(dir) {
 						console.log(dir);
 						$scope.dir=dir;
-						console.log($scope.dir);
-						console.log($scope.subject);
 						
-						document.getElementById("dropbox").className = "hidden";
-						document.getElementById("dropbox2").className = "";
+						if(dir.status!=undefined) //no file or folder in directory
+						{
+							document.getElementById("file").className = "hidden";
+							document.getElementById("folder").className = "hidden";
+							return;
+						}
 						
-						document.getElementById("dropbox3").innerHTML="";
-						document.getElementById("dropbox3").className = "";
+						document.getElementById("initial").className = "hidden";
 						
-					//document.getElementById("dropbox3").innerHTML="<li>hello</li>";
-					
+						var has_folder=false;
+						var has_file=false;
+						
 						for (var i in dir)
 						{
-							if(dir[i].folder=="false")
+							if(dir[i].folder=="false") //this is a file
 							{
-								document.getElementById("dropbox3").innerHTML=document.getElementById("dropbox3").innerHTML+"<a href=http://10.0.0.10:3000/lecturer/"+dir[i].path+" download><li  class='topcoat-list__item' style='background-color: #FFFFFF;'><div><img style='float:left; width 42px; height:45px; margin-right:10px;'class='movieimg' src='resources/img/1.png' /><strong style='font-size:17px;'></strong><p style='font-size:12px;'>"+dir[i].name+"</p></div><div style='clear:both;'></div></li></a>";
-								//alert(x);
-								//files.push(dir[i].name);
+								has_file=true;
+							}
+							
+							else
+							{
+								has_folder=true;
 							}
 						}
 						
+						if(has_file)
+						{
+							document.getElementById("file").className = "";
+						}
+						
+						else
+						{
+							document.getElementById("file").className = "hidden";
+						}
+						
+						if(has_folder)
+						{
+							document.getElementById("folder").className = "";
+						}
+						
+						else
+						{
+							document.getElementById("folder").className = "hidden";
+						}
 						
 						
-					/*	
-						var x=document.getElementById("dropbox");
-						x.innerHTML="";	
-
-						
-						$scope.html="<li  ng-show='ready' class='topcoat-list__item' style='background-color: #FFFFFF;'><strong style='font-size:17px;'>{{test}}</strong><p style='font-size:12px;'>{{item.status}}</p></div><div style='clear:both;'></div></li>";*/
+						document.getElementById("directory_options").className = "";
 		},
-				function(data) { //failure
-					console.log('WE ARE HAVING TROUBLE RETRIEVING PATH DATA');
-					$scope.statusmessage =  'WE ARE HAVING TROUBLE RETRIEVING PATH DATA';
-					$scope.ready =true;
-					$scope.conn = false;
-        		});
+			function(data) { //failure
+				console.log('WE ARE HAVING TROUBLE RETRIEVING PATH DATA');
+				$scope.statusmessage =  'WE ARE HAVING TROUBLE RETRIEVING PATH DATA';
+				$scope.ready =true;
+				$scope.conn = false;
+        	});
 		
-		};
-		/*
-		$http({method: 'POST', url: 'http://10.0.0.10:3000/dir', data: {'path' : path}}).
-					success(function(dir, status, headers, config) {
-						
-						//console.log(dir.names);
-						console.log(dir);
-						$scope.dir=dir;
-						console.log($scope.dir);
-						console.log($scope.subject);
-									
-						//document.getElementById("dropbox").className = "hidden";
-						//document.getElementById("dropbox2").className = "";
-						console.log($scope.dir);
-						var x=document.getElementById("dropbox");
-						x.innerHTML="";	
-
-						$scope.test="hel";
-						
-						$scope.html="<li  ng-show='ready' class='topcoat-list__item' style='background-color: #FFFFFF;'><strong style='font-size:17px;'>{{test}}</strong><p style='font-size:12px;'>{{item.status}}</p></div><div style='clear:both;'></div></li>";
-						/*
-						angular.element(document).injector().invoke(function($compile) {
-						var scope = angular.element($scope.hi).scope();
-						$compile($scope.hi)(scope);
-						});
-						
-						*
-						
-						
-						//console.log(dir);
-					}).
-					error(function(data, status, headers, config) {
-					
-					});*/
+		}
+		
+		//--------------------Goes up one level to previous folder-----------------------------//
+		$scope.goBack = function(){
+		
+			var path=path_history.pop();
 			
-			$scope.deleteNotice = function(id){
-				console.log(id);
-				
-				var url = 'http://10.0.0.10:3000/deletenotice';
-				
-				myNotices.post(url,{'id':id}).then(function(notices) {
-						console.log(notices);
-						$scope.notices=notices;
-						console.log($scope.notices);
-					
-				},
-				function(data) { //failure
-					console.log('WE ARE HAVING TROUBLE RETRIEVING YOUR NOTICES');
-					$scope.statusmessage =  'WE ARE HAVING TROUBLE RETRIEVING YOUR NOTICES';
-					$scope.ready =true;
-					$scope.conn = false;
-        		});
-				
+			if(path_history.length==0)
+			{
+				$scope.updateView(path, true);
 			}
+			
+			else
+			{
+				path=path_history.pop(); //pop another since the current directory will be stored as well
+				$scope.updateView(path);
+			}
+						
+		}
+		
+		//--------------------Performs sanity check for disallowed characters in folder name-----------------------------//
+		$scope.checkCharacters = function(){
+		
+			var s=$scope.folder_name;
+			
+			if(s==="" || s==null)
+			{
+				$scope.is_button_disabled=true;
+				return;
+			}
+						
+			var found_disallowed=false;
+			
+			for(var i=0; i<disallowed_characters.length; ++i)
+			{
+			
+				if(s.indexOf(disallowed_characters[i])!=-1)
+				{
+					found_disallowed=true;
+				}
+			}
+			
+			if(found_disallowed)
+			{
+				$scope.is_button_disabled=true;
+				document.getElementById("folder_error").className = "";
+			}
+			
+			else
+			{
+				$scope.is_button_disabled=false;
+				document.getElementById("folder_error").className = "hidden";
+			}
+						
+		}
+		
+		//--------------------Creates a new folder within working directory-----------------------------//
+		$scope.addFolder = function(folder){
+			
+			var path=path_history[(path_history.length-1)]+"/"+folder;
+			var url = ip+'/addfolder';
+			
+			myNotices.post(url,{'path':path}).then(function(status) {
+			
+				//should reload view here
+				
+				if(status.status=="success")
+				{
+					$scope.updateView(path_history[(path_history.length-1)]);
+					$scope.folder_name="";
+					$scope.is_button_disabled=true;
+				}
+						
+			},
+			function(data) { //failure
+				console.log('WE ARE HAVING TROUBLE ADDING THE FOLDER');
+				$scope.statusmessage =  'WE ARE HAVING TROUBLE ADDING THE FOLDER';
+				$scope.ready =true;
+				$scope.conn = false;
+        	});
+			
+		}
+		
+		//--------------------Removes a specific file within working folder-----------------------------//
+		$scope.removeFile = function(path){
+			console.log(path);
+			
+			var url = ip+'/removefile';
+			
+			myNotices.post(url,{'path':path}).then(function(status) {
+			
+				//should reload view here
+				
+				if(status.status=="success")
+				{
+					$scope.updateView(path_history[(path_history.length-1)]);
+				}
+						
+			},
+			function(data) { //failure
+				console.log('WE ARE HAVING TROUBLE DELETING THE FILE');
+				$scope.statusmessage =  'WE ARE HAVING TROUBLE DELETING THE FILE';
+				$scope.ready =true;
+				$scope.conn = false;
+        	});
+			
+		}
+		
+		//--------------------Removes folder and all subfolders and files-----------------------------//
+		$scope.removeFolder = function(path){
+			console.log(path);
+			
+			var url = ip+'/removefolder';
+			
+			myNotices.post(url,{'path':path}).then(function(status) {
+			
+				//should reload view here
+	
+				if(status.status=="success")
+				{
+					$scope.updateView(path_history[(path_history.length-1)]);
+				}
+						
+			},
+			function(data) { //failure
+				console.log('WE ARE HAVING TROUBLE DELETING THE FOLDER');
+				$scope.statusmessage =  'WE ARE HAVING TROUBLE DELETING THE FOLDER';
+				$scope.ready =true;
+				$scope.conn = false;
+        	});
+			
+		}
+		
+		//-------------------------------------------------------------------------------------//
+		//End of all functions pertaining to the directory listing
+		//-------------------------------------------------------------------------------------//
+		
+		
 		
 }						
