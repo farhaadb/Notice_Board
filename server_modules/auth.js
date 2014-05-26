93741961df7724107a1b5b6010bd1d05e421334a
Farhaad <farhaad.bux@gmail.com>
2014-05-27 00:24:48 +0200

    Adding lecturer and student authentication

--- server_modules/auth.js
+++ server_modules/auth.js
@@ -1,9 +1,23 @@
 module.exports = function() {
  return function checkAuth(req, res, next) {
-		if (!req.session.user_id) {
-			res.send('You are not authorized to view this page');
-		} else {
-		next();
+		
+		if((req.url).indexOf("/studentindex")!=-1)
+		{
+			console.log(req.session.student_id);
+			if (!req.session.student_id) {
+				res.send('You are not authorized to view this page');
+			} else {
+			next();
+			}
+		}
+		
+		else if((req.url).indexOf("/lecturerindex")!=-1)
+		{
+			if (!req.session.lecturer_id) {
+				res.send('You are not authorized to view this page');
+			} else {
+			next();
+			}
 		}
 	}
 };
