a3db6d641ae052e903cabcdb075ca7abd0e5742e
Farhaad <farhaad.bux@gmail.com>
2014-06-08 19:40:08 +0200

    Adding overlay.js

---
+++ lecturer/modules/lecturermodules/components/Overlay.js
@@ -0,0 +1,13 @@
+modalHide();
+
+function modalShow(){
+//alert('Show Now');
+document.getElementById('overlay').style.display='block';
+document.getElementById('fade').style.display='block'
+}
+
+ function modalHide (){
+//alert('Hide Now');
+document.getElementById('overlay').style.display='none';
+document.getElementById('fade').style.display='none';
+} 
